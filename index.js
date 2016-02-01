a0b392157fc1247483d210460737dc5c6e250757
Stephen Davies <steve9164@gmail.com>
2016-02-01 12:49:02 +1100

    Fix uri pathname and add some comments and debugging statements

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ require('mbtiles').registerProtocols(tilelive);
 module.exports = Hybrid;
 
 function Hybrid(uri, callback) {
-    console.log(uri);
+    //console.log(uri);
     // From node-mbtiles
     if (typeof uri === 'string') {
         uri = url.parse(uri, true);
@@ -46,11 +46,12 @@ function Hybrid(uri, callback) {
 
     var that = this;
 
-    this._open = nodefn.call(fs.readFile, uri.resolve(uri.pathname), 'utf8').then(function(json) {
+    this._open = nodefn.call(fs.readFile, uri.pathname, 'utf8').then(function(json) {
         config = JSON.parse(json);
         var promises = [];
         for (var i = 0; i < config.sources.length; i++) {
             var item = config.sources[i];
+            console.log('Adding ' + item.source);
             // Add each source to _sources
             promises.push(nodefn.call(tilelive.load, item.source).then(function(source) {
                 that._sources.push({
@@ -72,11 +73,13 @@ Hybrid.registerProtocols = function(tilelive) {
 
 
 Hybrid.prototype.open = function(callback) {
+    console.log('checking open');
     nodefn.bindCallback(this._open, callback);
 };
 
 // Allows in-place update of XML/backends.
 Hybrid.prototype.update = function(opts, callback) {
+    console.log('update called');
     /*// Unset maxzoom. Will be re-set on first getTile.
     this._maxzoom = undefined;
     // Unset type. Will be re-set on first getTile.
@@ -100,6 +103,7 @@ Hybrid.prototype.update = function(opts, callback) {
 };
 
 Hybrid.prototype.close = function(callback) {
+    console.log('closing');
     var promises = [];
     for (var i = 0; i < this._sources.length; i++) {
         var item = this._sources[i];
@@ -117,6 +121,7 @@ Hybrid.prototype.close = function(callback) {
 
 // Get the tile from the first listed source that supports the zoom level
 Hybrid.prototype.getTile = function(z, x, y, callback) {
+    console.log('Getting ' + [z,x,y].join('/') + '.pbf');
     for (var i = 0; i < this._sources.length; i++) {
         var item = this._sources[i];
         if (item.minZ <= z && z <= item.maxZ) {
@@ -126,14 +131,18 @@ Hybrid.prototype.getTile = function(z, x, y, callback) {
     callback(new Error('No sources for zoom level z=' + z));
 };
 
-
+// Hybrid is made to refer to the same set of data in multiple sources (pre-generated tiles and tiles generated as needed),
+// each source's info should be the same, hence returning info from the first source should be fine
 Hybrid.prototype.getInfo = function(callback) {
-    if (this._sources && this.sources.length > 0) {
+    console.log('getInfo called');
+    if (this._sources && this._sources.length > 0) {
+        this._sources[0].source.getInfo(console.log);
         return this._sources[0].source.getInfo(callback);
     }
     return callback(new Error('No sources'));
 };
 
 Hybrid.prototype.getIndexableDocs = function(pointer, callback) {
+    console.log('getIndexableDocs called');
     callback(new Error('getIndexableDocs not defined'));
 };
