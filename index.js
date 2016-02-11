7d058d29fc0cfbc55b0cde0306741df2ec06a198
Stephen Davies <steve9164@gmail.com>
2016-02-11 14:05:14 +1100

    Add tileIsCached to be used with steve9164/tilelive-cache

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var path = require('path');
 var mapnik = require('mapnik');
 
-var qs = require('querystring');
+
 var sm = new (require('sphericalmercator'))();
 var immediate = global.setImmediate || process.nextTick;
 var mapnik_pool = require('mapnik-pool');
@@ -10,12 +10,15 @@ var Pool = mapnik_pool.Pool;
 var os = require('os');*/
 
 var fs = require('fs');
+var qs = require('querystring');
 var when = require('when');
 var nodefn = require('when/node');
 
 var tilelive = require('tilelive');
 require('tilelive-bridge').registerProtocols(tilelive);
-require('mbtiles').registerProtocols(tilelive);
+var MBTiles = require('mbtiles'); // Need instanceof MBTiles
+MBTiles.registerProtocols(tilelive);
+//var tilecache = new (require("node-cache"))();
 
 
 module.exports = Hybrid;
@@ -49,19 +52,13 @@ function Hybrid(uri, callback) {
     this._open = nodefn.call(fs.readFile, uri.pathname, 'utf8').then(function(json) {
         config = JSON.parse(json);
         var promises = [];
-        for (var i = 0; i < config.sources.length; i++) {
-            var item = config.sources[i];
+        return when.map(config.sources, function(item) {
             console.log('Adding ' + item.source);
             // Add each source to _sources
-            promises.push(nodefn.call(tilelive.load, item.source).then(function(source) {
-                that._sources.push({
-                    source: source,
-                    minZ: item.minZ,
-                    maxZ: item.maxZ
-                });
-            }));
-        }
-        return when.all(promises);
+            return nodefn.call(tilelive.load, item.source).then(function(source) {
+                that._sources.push({ source: source, minZ: item.minZ, maxZ: item.maxZ });
+            });
+        });
     }).yield(this);
 
     nodefn.bindCallback(this._open, callback);
@@ -103,32 +100,33 @@ Hybrid.prototype.update = function(opts, callback) {
 };
 
 Hybrid.prototype.close = function(callback) {
-    console.log('closing');
-    var promises = [];
-    for (var i = 0; i < this._sources.length; i++) {
-        var item = this._sources[i];
-        promises.push(nodefn.call(item.close.bind(item)));
-    }
-
     var that = this;
-    when.all(promises).then(function() {
+    console.log('Closing');
+    when.map(this._sources, function(item) {
+        return nodefn.call(item.close.bind(item));
+    }).then(function() {
         // Release memory of sources
-        delete this._sources;
+        delete that._sources;
         callback();
     })
 
 };
 
-// Get the tile from the first listed source that supports the zoom level
-Hybrid.prototype.getTile = function(z, x, y, callback) {
-    console.log('Getting ' + [z,x,y].join('/') + '.pbf');
+function getSource(z, x, y) {
     for (var i = 0; i < this._sources.length; i++) {
         var item = this._sources[i];
         if (item.minZ <= z && z <= item.maxZ) {
-            return item.source.getTile(z, x, y, callback);
+            return item.source;
         }
     }
-    callback(new Error('No sources for zoom level z=' + z));
+    return null;
+}
+
+// Get the tile from the first listed source that supports the zoom level
+Hybrid.prototype.getTile = function(z, x, y, callback) {
+    console.log('Getting ' + [z,x,y].join('/') + '.pbf');
+    var source = getSource.call(this, z, x, y);
+    return (source) ? source.getTile(z, x, y, callback) : callback(new Error('No sources for zoom level z=' + z));
 };
 
 // Hybrid is made to refer to the same set of data in multiple sources (pre-generated tiles and tiles generated as needed),
@@ -136,8 +134,8 @@ Hybrid.prototype.getTile = function(z, x, y, callback) {
 Hybrid.prototype.getInfo = function(callback) {
     console.log('getInfo called');
     if (this._sources && this._sources.length > 0) {
-        this._sources[0].source.getInfo(console.log);
-        return this._sources[0].source.getInfo(callback);
+        this._sources[1].source.getInfo(console.log);
+        return this._sources[1].source.getInfo(callback);
     }
     return callback(new Error('No sources'));
 };
@@ -146,3 +144,8 @@ Hybrid.prototype.getIndexableDocs = function(pointer, callback) {
     console.log('getIndexableDocs called');
     callback(new Error('getIndexableDocs not defined'));
 };
+
+Hybrid.prototype.tileIsCached = function(z, x, y) {
+    var source = getSource.call(this, z, x, y);
+    return (source) ? source instanceof MBTiles : null;
+}
