fda08e59ff6658f07c322af1b0af17c08ce4bdb1
Stephen Davies <steve9164@gmail.com>
2016-05-13 16:08:13 +1000

    Fix errors in Hybrid.close() and refactor Hybrid.open

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-/*var url = require('url');
-var path = require('path');
-var mapnik = require('mapnik');
-
-
-var sm = new (require('sphericalmercator'))();
-var immediate = global.setImmediate || process.nextTick;
-var mapnik_pool = require('mapnik-pool');
-var Pool = mapnik_pool.Pool;
-var os = require('os');*/
-
 var fs = require('fs');
 var qs = require('querystring');
 var when = require('when');
@@ -46,10 +35,10 @@ function Hybrid(uri, callback) {
     if (!uri.query.batch) uri.query.batch = 100;
 
     this._sources = [];
+    this.open = false;
 
     var that = this;
-
-    this._open = nodefn.call(fs.readFile, uri.pathname, 'utf8').then(function(json) {
+    var p = nodefn.call(fs.readFile, uri.pathname, 'utf8').then(function(json) {
         config = JSON.parse(json);
         var promises = [];
         return when.map(config.sources, function(item) {
@@ -61,62 +50,33 @@ function Hybrid(uri, callback) {
                 that._sources.push({ source: source, minZ: item.minZ, maxZ: item.maxZ });
             });
         });
+    }).then(function() {
+        that.open = true;
     }).yield(this);
-
-    nodefn.bindCallback(this._open, callback);
+    nodefn.bindCallback(p, callback);
 }
 
 Hybrid.registerProtocols = function(tilelive) {
     tilelive.protocols['hybrid:'] = Hybrid;
 };
 
-
-Hybrid.prototype.open = function(callback) {
-    console.log('checking open');
-    nodefn.bindCallback(this._open, callback);
-};
-
-// Allows in-place update of XML/backends.
-Hybrid.prototype.update = function(opts, callback) {
-    console.log('update called');
-    /*// Unset maxzoom. Will be re-set on first getTile.
-    this._maxzoom = undefined;
-    // Unset type. Will be re-set on first getTile.
-    this._type = undefined;
-    this._xml = opts.xml;
-    this._readonly_map = new mapnik.Map(1,1);
-    var mopts = { strict: false, base: this._base + '/' };
-    this._readonly_map.fromString(this._xml,mopts,function(err) {
-        if (err) {
-            return callback(err);
-        }
-        this.close(function() {
-            this._map = mapnikPool.fromString(this._xml,
-                { size: 256, bufferSize: 256 },
-                mopts);
-            this._im = ImagePool(512);
-            return callback();
-        }.bind(this));
-    }.bind(this));*/
-    callback(new Error('update not defined'));
-};
-
 Hybrid.prototype.close = function(callback) {
     var that = this;
-    console.log('Closing');
     when.map(this._sources, function(item) {
-        return nodefn.call(item.close.bind(item));
+        return nodefn.call(item.source.close.bind(item.source));
     }).then(function() {
         // Release memory of sources
-        delete that._sources;
+        that._sources = null;
+        that.open = false;
         callback();
-    })
-
+    }).catch(function(err) {
+        callback(err);
+    });
 };
 
-function getSource(z, x, y) {
-    for (var i = 0; i < this._sources.length; i++) {
-        var item = this._sources[i];
+function getSource(sources, z, x, y) {
+    for (var i = 0; i < sources.length; i++) {
+        var item = sources[i];
         if (item.minZ <= z && z <= item.maxZ) {
             return item.source;
         }
@@ -126,28 +86,24 @@ function getSource(z, x, y) {
 
 // Get the tile from the first listed source that supports the zoom level
 Hybrid.prototype.getTile = function(z, x, y, callback) {
-    console.log('Getting ' + [z,x,y].join('/') + '.pbf');
-    var source = getSource.call(this, z, x, y);
+    if (!this.open) return callback(new Error('Hybrid data sources not yet loaded'));
+    var source = getSource(this._sources, z, x, y);
     return (source) ? source.getTile(z, x, y, callback) : callback(new Error('No sources for zoom level z=' + z));
 };
 
 // Hybrid is made to refer to the same set of data in multiple sources (pre-generated tiles and tiles generated as needed),
 // each source's info should be the same, hence returning info from the first source should be fine
 Hybrid.prototype.getInfo = function(callback) {
-    console.log('getInfo called');
+    if (!this.open) return callback(new Error('Hybrid data sources not yet loaded'));
     if (this._sources && this._sources.length > 0) {
-        this._sources[1].source.getInfo(console.log);
-        return this._sources[1].source.getInfo(callback);
+        this._sources[this._sources.length-1].source.getInfo(console.log);
+        return this._sources[this._sources.length-1].source.getInfo(callback);
     }
     return callback(new Error('No sources'));
 };
 
-Hybrid.prototype.getIndexableDocs = function(pointer, callback) {
-    console.log('getIndexableDocs called');
-    callback(new Error('getIndexableDocs not defined'));
-};
-
+// Used by steve9164/tilelive-cache
 Hybrid.prototype.tileIsCached = function(z, x, y) {
-    var source = getSource.call(this, z, x, y);
+    var source = getSource(this._sources, z, x, y);
     return (source) ? source instanceof MBTiles : null;
 }
